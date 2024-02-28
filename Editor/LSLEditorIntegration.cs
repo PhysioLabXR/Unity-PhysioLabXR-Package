dc3758a7a44df0d8052a56f7c4791e14ced31cef
Jace Li <yl4862@columbia.edu>
2024-02-27 19:29:28 -0500

    Publish package

---
+++ Editor/LSLEditorIntegration.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Linq;
+using UnityEngine.Assertions;
+
+namespace LSL4Unity.EditorExtensions
+{
+    public class LSLEditorIntegration
+    {
+        public static readonly string wikiURL = "https://github.com/xfleckx/LSL4Unity/wiki";
+        public static readonly string lib64Name = "liblsl64";
+        public static readonly string lib32Name = "liblsl32";
+        
+        public const string DLL_ENDING = ".dll";
+        public const string SO_ENDING = ".so";
+        public const string BUNDLE_ENDING = ".bundle";
+
+        [MenuItem("LSL/Show Streams")]
+        static void OpenLSLWindow()
+        {
+            var window = EditorWindow.GetWindow<LSLShowStreamsWindow>(true);
+
+            window.Init();
+
+            window.ShowUtility();
+        }
+    }
+}
---
+++ Editor/TimeSyncEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using LSL4Unity.Utils;
+
+
+namespace LSL4Unity.EditorExtensions
+{
+    [CustomEditor(typeof(TimeSync))]
+    public class TimeSyncEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+        }
+    }
+
+}
+
